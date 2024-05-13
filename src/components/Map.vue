b2564e570aa4aa44d80014ec4d380e477415dbf4
michaelkobetss <michael.kobetss@gmail.com>
2024-05-14 00:50:21 +0300

    0.0.4
    controls work in progress

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto mt-4">
     <div class="flex">
-      <div id="controls" class="w-2/5">
+      <div id="controls" class="controls">
         <label for="color">Color:</label>
         <input type="color" id="color" v-model="color" @change="updateColor" />
         <label for="opacity">Opacity:</label>
@@ -20,17 +20,24 @@
         >
           Export Geometry
         </button>
+        <button id="drawPolygon">Draw Polygon</button>
+  <button id="drawLineString">Draw LineString</button>
+  <button id="drawPoint">Draw Point</button>
+  <button id="delete">Delete</button>
       </div>
-      <div id="map" class="w-3/5"></div>
+      <div id="map" class="map"></div>
     </div>
+    
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue'
 import maplibregl from 'maplibre-gl'
-import MapboxDraw from '@mapbox/mapbox-gl-draw'
+
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
+
 import * as turf from '@turf/turf'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
@@ -38,6 +45,7 @@ const MAP_STYLE_URL =
   'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'
 const MAP_CENTER = [30.5, 50.5]
 const MAP_ZOOM = 13
+
 const polygon = turf.polygon([
   [
     [30.499318908327723, 50.50151942932925],
@@ -60,9 +68,22 @@ export default {
     const map = ref(null)
     const color = ref('#088000')
     let opacity = ref(0.8)
+    let draw = null
 
     onMounted(() => {
       initializeMap()
+      document.getElementById('drawPolygon').addEventListener('click', function() {
+    draw.changeMode('draw_polygon');
+  });
+  document.getElementById('drawLineString').addEventListener('click', function() {
+    draw.changeMode('draw_line_string');
+  });
+  document.getElementById('drawPoint').addEventListener('click', function() {
+    draw.changeMode('draw_point');
+  });
+  document.getElementById('delete').addEventListener('click', function() {
+    draw.changeMode('delete');
+  });
     })
 
     function initializeMap() {
@@ -73,19 +94,21 @@ export default {
         zoom: MAP_ZOOM
       })
 
+      draw = new MapboxDraw({
+        displayControlsDefault: false,
+        controls: {
+          polygon: true,
+          trash: true
+        }
+      })
+      
+      
       map.value.on('load', () => {
-        addMarkerToMap()
         addPolygonToMap(polygon)
         addPointToMap(point)
         addLineToMap(line)
       })
-
-
-      
-    }
-
-    function addMarkerToMap() {
-      new maplibregl.Marker().setLngLat(MAP_CENTER).addTo(map.value)
+      map.value.addControl(draw, 'top-right')
     }
 
     function addPolygonToMap(polygon) {
@@ -110,6 +133,7 @@ export default {
         type: 'geojson',
         data: point
       })
+
       map.value.addLayer({
         id: 'point',
         type: 'circle',
@@ -122,11 +146,12 @@ export default {
       })
     }
 
-    function addLineToMap() {
+    function addLineToMap(line) {
       map.value.addSource('line', {
         type: 'geojson',
         data: line
       })
+
       map.value.addLayer({
         id: 'line',
         type: 'line',
@@ -162,13 +187,11 @@ export default {
         return
       }
 
-      const polygonData = map.value.getSource('polygon')._data
-      const pointData = map.value.getSource('point')._data
-      const lineData = map.value.getSource('line')._data
+      const drawnData = draw.getAll()
 
       const combinedData = {
         type: 'FeatureCollection',
-        features: [polygonData, pointData, lineData]
+        features: [...drawnData.features]
       }
 
       const blob = new Blob([JSON.stringify(combinedData)], {
@@ -179,6 +202,7 @@ export default {
       link.download = 'geometry.geojson'
       link.click()
     }
+
     return {
       exportGeometry,
       color,
@@ -211,7 +235,7 @@ export default {
   margin-bottom: 10px; /* Adjust as needed */
 }
 
-.container {
-  margin-top: 60px;
+.mapboxgl-ctrl-top-right {
+  z-index: 9999 !important;
 }
 </style>
